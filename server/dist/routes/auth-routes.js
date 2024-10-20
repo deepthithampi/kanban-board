682568b2af4cbf6a28c99ef5cadc2d63682c8c95
Deepthi Sandeep <deepththampi@hotmail.com>
2024-10-20 14:16:26 -0500

    updated gitignore in root too

---
+++ server/dist/routes/auth-routes.js
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+export const login = async (req, res) => {
+    // TODO: If the user exists and the password is correct, return a JWT token
+    // Extract username and password from request body
+    const { username, password } = req.body;
+    // Find the user in the database by username
+    const user = await User.findOne({
+        where: { username },
+    });
+    // If user is not found, send an authentication failed response
+    if (!user) {
+        return res.status(401).json({ message: 'Authentcation Failed' });
+    }
+    // Compare the provided password with the stored hashed password
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+        return res.status(401).json({ message: 'Authentication Failed' });
+    }
+    // Get the secret key from environment variables
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+    // Generate a JWT token for the authenticated user
+    const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+    // Send the token as a JSON response
+    return res.json({ token });
+};
+const router = Router();
+// POST /login - Login a user
+router.post('/login', login);
+export default router;
